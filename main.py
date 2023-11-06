f5f81898ed83bb64d0c5567d1b24311ee57b8e1e
czl <chengzongli.ee10@nycu.edu.tw>
2023-11-06 19:21:06 +0800

    Entry point added

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import pygame
+# from pygame.locals import QUIT
 
-if __name__ == 'main':
-    print('This is the entry of the game.')
+RESOLUTION = (1600, 900)
+CAPTION = "Boomerang"
+
+running = True
+
+# Initialize pygame
+pygame.init()
+window_surface = pygame.display.set_mode(RESOLUTION)
+pygame.display.set_caption(CAPTION)
+window_surface.fill((255, 255, 255))
+
+# Game loop
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            ...
+
+pygame.quit()
